12c74c290870f810fa68dd9cea597740e100ccbd
baramatsubonzo <matsubara.shota@moneyforward.co.jp>
2020-08-05 16:58:03 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :posts
 end

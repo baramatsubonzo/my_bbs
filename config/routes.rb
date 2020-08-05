21b209ef5e1430d9ed00d5bc26745e06ad22f784
baramatsubonzo <matsubara.shota@moneyforward.co.jp>
2020-08-05 16:43:33 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'posts#index'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :users
   resources :posts
 end

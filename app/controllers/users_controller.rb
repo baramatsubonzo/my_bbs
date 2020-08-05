5cafa3e1a5e92d256fff9e53286efac6e7741818
baramatsubonzo <matsubara.shota@moneyforward.co.jp>
2020-08-05 16:05:32 +0900

    ユーザー一覧ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  # def show
+  #   @user = User.find(params[:id])
+  # end
+
   def new
     @user = User.new
   end
 
-  def crete
+  def create
     @user = User.new(user_params)
 
     if @user.save
@@ -13,15 +21,28 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def show
-  end
+  # def update
+  #   @user = User.find(params[:id])
 
-  def index
-  end
+  #   if @user.update(user_params)
+  #     redirect_to user_path(@user), notice: "ユーザー「#{@user.name}を更新しました"
+  #   else
+  #     render :new
+  #   end
+  # end
 
+  # def destroy
+  #   @user = User.find(params[:id])
+  #   @user.destroy
+  #   redirect_to users_path, notice:  "ユーザー「#{@user.name}を削除しました」"
+  # end
+
+  private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end

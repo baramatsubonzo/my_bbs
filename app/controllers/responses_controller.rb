75aaedf6115f815aa8936e901f8b8c08fb0deea7
baramatsubonzo <matsubara.shota@moneyforward.co.jp>
2020-08-07 16:08:31 +0900

    投稿を表示できるようにした

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,16 +1,19 @@
 class ResponsesController < ApplicationController
-  def create
-    @response = Response.new(response_params)
-    @response.user_id = current_user.id
-    if @response.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
+  before_action :set_post
 
+  def create
+    post = Post.find(params[:post_id])
+    response = current_user.responses.build(response_params)
+    response.post_id = post.id
+    response.save
+    redirect_to post_path(post.id)
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def response_params
     params.require(:response).permit(:content)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :posts
-  has_many :response
+  has_many :responses
 end
